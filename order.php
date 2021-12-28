42d4c17015e0c51aa9dc668b5e782e0d41a86473
Queena Tao <queena113@gmail.com>
2021-12-28 22:34:53 +0800

    fix serch filter detail #2

--- order.php
+++ order.php
@@ -1,7 +1,7 @@
 echo '這是訂單搜尋功能';
-ehco 'This is bug 1';
+<!-- ehco 'This is bug 1'; -->
 echo '這是訂單篩選功能';
 echo '這是訂單明細功能'; 
 
-echo 'This is bug 2';
-echo 'This is bug 3';
+<!-- echo 'This is bug 2'; -->
+<!-- echo 'This is bug 3'; -->

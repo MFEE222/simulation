95d0febafd03d0221c298198cc728940a4b9ba18
chiang-song-lang <afga3780@gmail.com>
2021-12-28 22:41:51 +0800

    uptade login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 echo '這是帳號密碼登入功能';
-ehco 'This is bug 1';
+<!-- ehco 'This is bug 1'; -->
 echo '這是忘記密碼功能';
 echo '這是記住我功能'; 
 
-echo 'This is bug 2';
-echo 'This is bug 3';
+<!-- echo 'This is bug 2'; -->
+<!-- echo 'This is bug 3'; -->
